66e58279b6919a5425b73be284dfd02400971b4d
Jayant-stack <jayanthalapati@gmail.com>
2025-02-18 21:22:09 -0500

    Leetcode 162. Peak Element.

---
+++ DSA/Leetcode/BinarySearch/FindPeakElement.java
@@ -0,0 +1,25 @@
+///Find Peak Element
+/// 
+/// Leetcode 162. Find Peak Element
+/// https://leetcode.com/problems/find-peak-element/
+
+package DSA.Leetcode.BinarySearch;
+
+public class FindPeakElement {
+    public int findPeakElement(int[] nums){
+        int left =0;
+        int right = nums.length -1;
+
+        
+        while(left< right){
+            int mid = left + (right - left)/2;
+            if(nums[mid] > nums[mid+1]){
+                right = mid;
+            }else{
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+    
+}
